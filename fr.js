97acdc628d9c0800ddf5285c441d094365cb03b1
TheSalfy <38163392+TheSalfy@users.noreply.github.com>
2018-04-08 21:27:19 +0200

    Update fr.js
    should be good

--- fr.js
+++ fr.js
@@ -2,7 +2,7 @@ module.exports = {
   title: 'Utilisation des commandes',
   titleError: 'Erreur :warning:',
   role: {
-    message: 'z/role message <messageID> ~~  ~~ Pour utiliser un message déjà éxistant\nor\nz/role message <message to send> ~~  ~~ Pour que le bot envoie et utilise le message que vous avez demandé\n\nPour avoir l\'ID du message, jetez un coup d'oeil ici :',
+    message: 'z/role message <messageID> ~~  ~~ Pour utiliser un message déjà éxistant\nor\nz/role message <message à envoyer> ~~  ~~ Pour que le bot envoie et utilise le message que vous avez demandé\n\nPour avoir l\'ID du message, jetez un coup d\'oeil ici :',
     messageUnableToSend: 'Impossible d\'envoyer le message dans <#',
     messageSetTitle: 'Le message a été posté',
     messageSetDesc: 'Le message a été posté avec l\'ID de',
@@ -14,7 +14,7 @@ module.exports = {
     channelSetTitle: 'Le salon a été assigné avec succès.',
     channelSetDesc: 'Le salon a été assigné avec l\'ID ',
     add: 'z/role add :emoji: @role\nor\nz/role add :emoji: nom du rôle',
-    noChannel: 'Utilisez tout d'abord `z/role channel` et enfin cette commande `z/role message`',
+    noChannel: 'Utilisez tout d\'abord `z/role channel` et enfin cette commande `z/role message`',
     roleNull: 'Ce n\'est pas un rôle valide',
     roleNope: 'Le rôle donné n\'a pas été assigné avec Zira ou bien il n\'a pas été assigné à cet ID de message.',
     emojiUsed: 'Cet émoji est déjà utilisé sur ce message.',
@@ -35,10 +35,10 @@ module.exports = {
   },
   help: [{
     name: 'Commandes',
-    value: '**z/role message** ~~  ~~ message pour réagir dans\n**z/role channel** ~~  ~~ salon pour la réaction\n**z/role add** ~~  ~~ ajouter un rôle avec un émoji\n**z/role del** ~~  ~~ supprimer un role\n**z/role edit** ~~  ~~ modifier un message envoyé par Zira\n**z/role list** ~~  ~~ liste des rôles utilisés\n**z/autorole** ~~  ~~ donner aux utilisateurs et aux bots un rôle lorsqu'ils rejoignent\n**z/log** ~~  ~~ enregister/log  quand Zira donne ou supprime un role\n\n**z/lang** ~~  ~~ Selectionner la langue\n**z/reset** ~~  ~~ Réinitialiser les paramètres du bot\n**z/info** ~~  ~~ informations à propos de Zira\n**z/ping** ~~  ~~ Affiche le ping de Zira\n**z/invite** ~~  ~~ Pour inviter Zira',
+    value: '**z/role message** ~~  ~~ message pour réagir dans\n**z/role channel** ~~  ~~ salon pour la réaction\n**z/role add** ~~  ~~ ajouter un rôle avec un émoji\n**z/role del** ~~  ~~ supprimer un role\n**z/role edit** ~~  ~~ modifier un message envoyé par Zira\n**z/role list** ~~  ~~ liste des rôles utilisés\n**z/autorole** ~~  ~~ donner aux utilisateurs et aux bots un rôle lorsqu\'ils rejoignent\n**z/log** ~~  ~~ enregister/log  quand Zira donne ou supprime un role\n\n**z/lang** ~~  ~~ Selectionner la langue\n**z/reset** ~~  ~~ Réinitialiser les paramètres du bot\n**z/info** ~~  ~~ informations à propos de Zira\n**z/ping** ~~  ~~ Affiche le ping de Zira\n**z/invite** ~~  ~~ Pour inviter Zira',
   }, {
     name: 'Liens',
-    value: '[Guide pour configurer Zira](https://demo.zira.pw)\n[Serveur d'Aide](https://zira.pw/support)\n[Tableau de Bord de Zira](https://zira.pw)\n[N\'hésitez pas à faire des dons pour que Zira continue de fonctionner](https://www.patreon.com/HazedSPaCEx)\n[Translate Zira](https://github.com/HazedSPaCEx/Zira-Translations)'
+    value: '[Guide pour configurer Zira](https://demo.zira.pw)\n[Serveur d\'Aide](https://zira.pw/support)\n[Tableau de Bord de Zira](https://zira.pw)\n[N\'hésitez pas à faire des dons pour que Zira continue de fonctionner](https://www.patreon.com/HazedSPaCEx)\n[Translate Zira](https://github.com/HazedSPaCEx/Zira-Translations)'
   }],
   perm: {
     noPerm: 'Vous devez être le propriétaire du serveur ou posséder la permission `Gérer les rôles` pour utiliser ces commandes.',
@@ -57,7 +57,7 @@ module.exports = {
     user: `z/autorole user @role\nor\nz/autorole user role name\n\nDo \`z/autorole user |reset\` pour que les utilisateurs ne recoivent plus de rôles en rejoignant.`,
     botSet1: `Le rôle du bot a été configuré à <@&`,
     botSet2: '>',
-    botReset: 'Zira ne donnera plus de rôles lorsqu'un bot rejoindra le serveur.',
+    botReset: 'Zira ne donnera plus de rôles lorsqu\'un bot rejoindra le serveur.',
     bot: `z/autorole bot @role\nor\nz/autorole bot role name\n\nDo \`z/autorole bot |reset\` pour que les bots ne recoivent plus de rôles en rejoignant.`,
     yes: 'Yes',
     no: 'No',
@@ -79,6 +79,6 @@ module.exports = {
     noPerm: 'Vous devez être le propriétaire du serveur pour executer cette commande.',
     question: 'Êtes vous sûr de vouloir réinitialiser la configuration du serveur ?\n**TOUTE** la configuration enregistrée de Zira sera réinitialisée.\n\n*oui ou non*',
     oui: 'La configuration a été réinitialisée.',
-    non: 'La configuration n'a pas été réinitialisée.'
+    non: 'La configuration n\'a pas été réinitialisée.'
   }
 }
