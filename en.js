988bd25bc55012b8f25a1f9c8269aefb71f14554
Unknown <livewire192@gmail.com>
2018-04-28 09:04:33 -0700

    new lang file

--- en.js
+++ en.js
@@ -1,129 +1,170 @@
+'use strict';
+
 module.exports = {
   title: 'Command Usage',
   titleError: 'Error :warning:',
-  role: {
-    message: 'z/role message <messageID> ~~  ~~ To use an existing message\nor\nz/role message <message to send> ~~  ~~ To make the bot send and use the message you provided\n\nTo get a message ID, take a look at this:',
-    messageUnableToSend: 'Unable to send message to <#',
-    messageSetTitle: 'Message Set',
-    messageSetDesc: 'Message set with the ID of ',
-    messageNonDigit: 'Your message ID can\'t have any non-digit characters in it.',
-    messageErrorID: 'Can\'t find a message with that ID',
-    setupChannelFirst: 'Please setup a channel first, using \`z/role channel\`',
-    channel: 'z/role channel #channel',
-    channelNull: 'That is not a valid channel',
-    channelSetTitle: 'Channel Set',
-    channelSetDesc: 'Channel set with the ID of ',
-    add: 'z/role add :emoji: @role\nor\nz/role add :emoji: role name',
-    noChannel: 'Please use `z/role channel` first, then `z/role message`, then use this command.',
-    roleNull: 'That is not a valid role',
-    roleNope: 'The role given was either not setup with Zira, or not set on that message ID.',
-    emojiUsed: 'Emoji already in use on this message.',
-    roleUsed: 'Role already in use on this message.',
-    roleSetTitle: 'Role Set',
-    roleSetDesc1: 'Set <@&',
-    roleSetDesc2: '> and can be added with ',
-    messageOrChannelError: 'Please make sure that the message(s) and channel are setup properly, that the bot has permission to see the channel and react on the message(s) set.',
-    del: 'z/role del <message ID> @role\nor\nz/role del <message ID> role name\n\nTo get the message ID that a role is on, do `z/role list`',
-    roleDelTitle: 'Role Removed',
-    roleDelDesc1: 'Removed ',
-    roleDelDesc2: ' and its corresponding emoji ',
-    noRoles: 'No roles have been setup yet with `z/role add`',
-    edit: 'z/role edit <message ID> <new message>',
-    editid: 'That message ID has not been setup through Zira.',
-    cannotEdit: 'Zira can\'t edit that message as Zira was not the author of it.',
-    editSuc: 'The message was successfully edited.'
+  titleComp: 'Complete :white_check_mark:',
+  unknownChannel: 'Unknown channel.',
+  unknownRole: 'Unknown role.',
+  unknownMessage: 'Unknown message.',
+  unknownEmoji: 'Unknown emoji.',
+  noChannel: ['Please set a channel first with **', 'channel**.'],
+  noMessage: ['Please set a message to use with **', 'message**.'],
+  help: {
+    commandsTitle: 'Commands',
+    commands: {
+      channel: 'set channel you want to use\n',
+      message: 'set the message you want to use\n',
+      add: 'add a role with an emoji\n',
+      toggle: 'add a role that can be toggled\n',
+      remove: 'remove a role that has been set\n',
+      edit: 'edit a message sent by Zira\n',
+      list: 'list the roles being used\n',
+      autorole: 'give users and bots a role on join\n',
+      log: 'log when Zira gives/takes a role\n',
+      join: 'sends a message when a user joins\n',
+      leave: 'sends a message when a user leaves\n',
+      lang: 'set locale\n',
+      reset: 'reset guild config\n',
+      info: 'info about Zira\n',
+      faq: 'frequently asked questions\n',
+      ping: 'Zira\'s ping\n',
+      invite: 'invite Zira',
+    },
+    categories: {
+      role: 'Roles',
+      logs: 'Logging',
+      misc: 'Miscellaneous',
+    },
+    linksTitle: 'Links',
+    links: '[Guide to setup Zira](https://demo.zira.pw)\n[Support Guild](https://zira.pw/support)\n[Zira\'s Dashboard](https://zira.pw)\n[Donate to keep Zira running](https://www.patreon.com/HazedSPaCEx)\n[Translate Zira](https://github.com/HazedSPaCEx/Zira-Translations)',
+    news: 'News',
   },
-  help: [{
-    name: 'Commands',
-    value: '**z/role channel** ~~  ~~ channel for the reaction\n**z/role message** ~~  ~~ message to react on\n**z/role add** ~~  ~~ add a role with an emoji\n**z/role del** ~~  ~~ delete a role\n**z/role edit** ~~  ~~ edit a message sent by Zira\n**z/role list** ~~  ~~ list roles being used\n**z/autorole** ~~  ~~ give users and bots a role on join\n**z/log** ~~  ~~ log when Zira gives/takes a role\n**z/join** ~~  ~~ sends a message when a user joins\n**z/leave** ~~  ~~ sends a message when a user leaves\n\n**z/lang** ~~  ~~ set locale\n**z/reset** ~~  ~~ reset server settings\n**z/info** ~~  ~~ info about Zira\n**z/faq** ~~  ~~ frequently asked questions\n**z/ping** ~~  ~~ Zira\'s ping\n**z/invite** ~~  ~~ invite Zira',
-  }, {
-    name: 'Links',
-    value: '[Guide to setup Zira](https://demo.zira.pw)\n[Support Server](https://zira.pw/support)\n[Zira\'s Dashboard](https://zira.pw)\n[Donate to keep Zira running](https://www.patreon.com/HazedSPaCEx)\n[Translate Zira](https://github.com/HazedSPaCEx/Zira-Translations)'
-  }],
+  channel: {
+    help: 'channel #channel** ~~-~~ set the channel that will be used',
+    set: 'Channel set to <#',
+  },
+  message: {
+    help: 'message [[message ID](https://support.discordapp.com/hc/en-us/articles/206346498-Where-can-I-find-my-User-Server-Message-ID-) | message]** ~~-~~ use an existing message or create a new one',
+    cannotSend: ['Unable to send a message to <#', '> because I lack the `Send Messages` permission.'],
+    cannotRead: ['Unable to send a message to <#', '> because I lack the `Read Messages` or `Read Message History` permission.'],
+    unknown: 'Unable to send message.',
+    cannotReadID: ['Unable to get the message from <#', '> because I lack the `Read Messages` or `Read Message History` permission.'],
+    unknownID: 'Unable to set message ID.',
+    set: 'Message set with the ID of ',
+  },
+  add: {
+    help: 'add [emoji] [@role | role name]** ~~-~~ add a role with an emoji to the message set',
+    cannotRead: ['Unable to react to the message in <#', '> because I lack the `Read Messages` or `Read Message History` permission.'],
+    cannotReact: ['Unable to react to the message in <#', '> because I lack the `Add Reaction` permission.'],
+    unknown: 'Unable to react to the message in <#',
+    set: ['<@&', '> added with the emoji ', '\n\n**NOTE:** The `Zira` role needs to be above the role you\'ve set and needs to have `Manage Roles` permission to give this role.'],
+    emoji: 'That emoji is already in use on this message.',
+    role: 'That role is already in use on this message.',
+  },
+  toggle: {
+    help: 'toggle [emoji] [@role | role name]** ~~-~~ add a role with an emoji to the message set',
+    image: 'https://stuff.zira.pw/files/1524890971185.gif', /* I will replace this with a gif that is fast as my internet and laptop is slow */
+  },
   perm: {
-    noPerm: 'You need be the owner of the server, or have the `MANAGE_ROLES` perm to use these commands.',
-    noLangPerm: 'You need be the owner of the server, or have the `MANAGE_GUILD` perm to change the bot\'s language.',
-    noGuildPerm: 'You need be the owner of the server, or have the `MANAGE_GUILD` permission to use this command.'
+    noPerm: 'You need be the owner of the guild, or have the `MANAGE_ROLES` perm to use these commands.',
+    noLangPerm: 'You need be the owner of the guild, or have the `MANAGE_GUILD` perm to change the bot\'s language.',
+    noGuildPerm: 'You need be the owner of the guild, or have the `MANAGE_GUILD` permission to use this command.',
   },
   lang: {
     langUpdate: 'Language updated.',
-    translate: 'If you want Zira in a language that isn\'t on the list above,\nyou can help translate it [here](https://github.com/HazedSPaCEx/Zira-Translations)'
-  },
-  auto: {
-    title: 'Auto Role',
-    enable: `Auto role is now enabled.`,
-    disable: `Auto role is now disabled.`,
-    userSet1: `User role set to <@&`,
-    userSet2: '>',
-    userReset: 'Zira will no longer give roles to a user upon joining.',
-    user: `z/autorole user @role\nor\nz/autorole user role name\n\nDo \`z/autorole user |reset\` to stop giving users a role upon joining.`,
-    botSet1: `Bot role set to <@&`,
-    botSet2: '>',
-    botReset: 'Zira will no longer give roles to a bot upon joining.',
-    bot: `z/autorole bot @role\nor\nz/autorole bot role name\n\nDo \`z/autorole bot |reset\` to stop giving bots a role upon joining.`,
-    yes: 'Yes',
-    no: 'No',
-    notsetup: 'Not setup',
-    showTitle: 'Auto role config',
-    showEnable: 'Enabled ~~  ~~ ',
-    showUser: 'User role ~~  ~~ ',
-    showBot: 'Bot role ~~  ~~ ',
-    helpTitle: 'Auto role commands',
-    helpDesc: `z/autorole toggle ~~  ~~ Enable or disable auto roles\nz/autorole user ~~  ~~ Set the user's role\nz/autorole bot ~~  ~~ Set the bot's role\nz/autorole show ~~  ~~ Shows the config`
+    translate: 'If you want Zira in a language that isn\'t on the list above,\nyou can help translate it [here](https://github.com/HazedSPaCEx/Zira-Translations)',
   },
   log: {
-    title: "Log Command",
-    desc: "**z/log #channel** ~~  ~~ set the channel for logging\n\nTo stop logging, do z/log |reset\n\nIf Zira has insufficient permissions to send messages to the setup channel, your guild might have access to this feature revoked.",
-    set: ["Log channel set to ", "\nNOTE: If Zira is not able to send to the channel, logging will be disabled."],
-    reset: "I will no longer log to "
-  },
-  reset: {
-    noPerm: 'You need be the owner of the server to use this command.',
-    question: 'Are you sure you want to reset the server config?\n**EVERYTHING** setup through Zira will be reset.\n\n*yes or no*',
-    yes: 'Config has been reset.',
-    no: 'Nothing has been reset.'
+    title: 'Log Command',
+    help: ['log #channel** ~~-~~ set the channel for logging\n\nTo stop logging, do **', 'log stop**\n\nAn example of a message is below'],
+    set: ['Log channel set to <#', '>\n\n**NOTE:** If Zira is not able to send to the channel, logging will be disabled.'],
+    stop: 'Log messages will no longer be sent.',
+    image: 'https://stuff.zira.pw/files/logExample.png',
+    give: [' reacted ', ' to get '],
+    remove: [' unreacted ', ' to remove '],
   },
   faq: {
     title: 'Frequently Asked Questions',
     fields: [{
       name: 'Whenever I do commands, Zira doesn\'t send messages, what do I do?',
-      value: 'Make sure Zira has permission to embed links and send messages in the channel you are trying the commands in.'
+      value: 'Make sure Zira has permission to embed links and send messages in the channel you are trying the commands in.',
     }, {
       name: 'Zira says `"Can\'t find a message with that ID"` or `"Unknown message ID"`, what do I do?',
-      value: 'Make sure that the message ID is in the channel you\'ve set, and that Zira is able to read the channel'
+      value: 'Make sure that the message ID is in the channel you\'ve set, and that Zira is able to read the channel',
     }, {
       name: 'Even though I set it up properly, Zira doesn\'t give roles.',
-      value: 'Make sure Zira has permission to manage roles, and that the "Zira" role is above the roles you\'ve setup.'
+      value: 'Make sure Zira has permission to manage roles, and that the "Zira" role is above the roles you\'ve setup.',
     }, {
       name: 'Is Zira able to remove emojis that don\'t have a corresponding role setup with them?',
-      value: 'Yes, but you must allow Zira to `manage messages` in the channel you\'ve setup.'
+      value: 'Yes, but you must allow Zira to `manage messages` in the channel you\'ve setup.',
     }, {
       name: 'Is Zira able to handle more than one message?',
-      value: 'Yes, just set another message, and add roles to that message.'
+      value: 'Yes, just set another message, and add roles to that message.',
     }, {
       name: 'Is Zira able to handle more than one channel?',
-      value: 'Yes, just set another channel, then message, etc.'
+      value: 'Yes, just set another channel, then message, etc.',
     }, {
       name: 'How many roles are allowed per message?',
-      value: 'Since Discord only allows 20 emojis per message, 20 roles are allowed per message.'
-    }]
+      value: 'Since Discord only allows 20 emojis per message, 20 roles are allowed per message.',
+    }],
   },
   join: {
-    help: '**z/join channel** ~~  ~~ set the channel for join messages\n**z/join message** ~~  ~~ message to be sent to the channel\n**z/join toggle** ~~  ~~ toggle whether or not to send a message\n\nIf Zira has insufficient permissions to send messages to the setup channel, your guild might have access to this feature revoked.',
-    on: 'When a user joins, a message will be sent to the setup channel.',
-    off: 'No messages will be sent.',
-    channel: ['Join channel set to <#', '>'],
-    channelUsage: '**z/join channel #channel** ~~  ~~ set the channel for join messages',
-    messageUsage: '**z/join message** ~~  ~~ set the message to be sent on user join\n\nYou can use these placeholders in the message:\n$user ~~  ~~ username\n$mention ~~  ~~ user mention\n$server ~~  ~~ server name\n$membercount ~~  ~~ new member count',
-    message: 'Join message set to:'
+    title: 'Join Command',
+    help: ['join channel** ~~-~~ set the channel that messages will be sent to\n**', 'join message** ~~-~~ set the message that will be sent when a user joins\n\nYou can use these placeholders in the message:\n$user ~~-~~ username\n$mention ~~-~~ user mention\n$guild ~~-~~ guild name\n$membercount ~~-~~ new member count'],
+    channel: ['join channel #channel** ~~-~~ set the channel that join messages will be sent to\n\nTo stop sending to the channel set, do **', 'join channel stop**'],
+    stop: 'Join messages will stop being sent.',
+    setChannel: ['Channel set to <#', '>\n\n**NOTE:** If Zira is not able to send to the channel, join messages will be disabled.'],
+    message: 'join message [message]** ~~-~~ set the message that will be sent when a user joins\n\nYou can use these placeholders in the message:\n$user ~~-~~ username\n$mention ~~-~~ user mention\n$guild ~~-~~ guild name\n$membercount ~~-~~ new member count',
+    setMessage: 'Join message set to:\n',
   },
   leave: {
-    help: '**z/leave channel** ~~  ~~ set the channel for leave messages\n**z/leave message** ~~  ~~ message to be sent to the channel\n**z/leave toggle** ~~  ~~ toggle whether or not to send a message\n\nIf Zira has insufficient permissions to send messages to the setup channel, your guild might have access to this feature revoked.',
-    on: 'When a user leaves, a message will be sent to the setup channel.',
-    off: 'No messages will be sent.',
-    channel: ['Leave channel set to <#', '>'],
-    channelUsage: '**z/leave channel #channel** ~~  ~~ set the channel for leave messages',
-    messageUsage: '**z/leave message** ~~  ~~ set the message to be sent on user leave\n\nYou can use these placeholders in the message:\n$user ~~  ~~ username\n$mention ~~  ~~ user mention\n$server ~~  ~~ server name\n$membercount ~~  ~~ new member count',
-    message: 'Leave message set to:'
-  }
-}
+    title: 'Leave Command',
+    help: ['leave channel** ~~-~~ set the channel that messages will be sent to\n**', 'leave message** ~~-~~ set the message that will be sent when a user leave\n\nYou can use these placeholders in the message:\n$user ~~-~~ username\n$mention ~~-~~ user mention\n$guild ~~-~~ guild name\n$membercount ~~-~~ new member count'],
+    channel: ['leave channel #channel** ~~-~~ set the channel that leave messages will be sent to\n\nTo stop sending to the channel set, do **', 'leave channel stop**'],
+    stop: 'Leave messages will stop being sent.',
+    setChannel: ['Channel set to <#', '>\n\n**NOTE:** If Zira is not able to send to the channel, leave messages will be disabled.'],
+    message: 'leave message [message]** ~~-~~ set the message that will be sent when a user leaves\n\nYou can use these placeholders in the message:\n$user ~~-~~ username\n$mention ~~-~~ user mention\n$guild ~~-~~ guild name\n$membercount ~~-~~ new member count',
+    setMessage: 'Leave message set to:\n',
+  },
+  auto: {
+    title: 'Auto Role',
+    help: ['autorole show** ~~-~~ show the current roles set\n**', 'autorole user** ~~-~~ add or remove roles that should be given on user join\n**', 'autorole bot** ~~-~~ add or remove roles that should be given on bot join'],
+    user: 'autorole user [@role | role name]** ~~-~~ add or remove a role that should be given when a user joins\n\nTo remove a role that has been set do the above command.',
+    userAdd: ['Added role <@&', '> that will be given when a user joins.\n\n**NOTE:** The `Zira` role needs to be above the role you\'ve set and needs to have `Manage Roles` permission to give this role.'],
+    userRemove: ['Removed role <@&', '> from being given when a user joins.'],
+    botAdd: ['Added role <@&', '> that will be given when a bot is invited.\n\n**NOTE:** The `Zira` role needs to be above the role you\'ve set and needs to have `Manage Roles` permission to give this role.'],
+    botRemove: ['Removed role <@&', '> from being given when a bot invited.'],
+    bot: 'autorole bot [@role | role name]** ~~-~~ add or remove a role that should be given when a bot is invited\n\nTo remove a role that has been set do the above command.',
+    showUser: 'User Roles',
+    showBot: 'Bot Roles',
+    no: 'No roles set.',
+  },
+  reset: {
+    title: 'Config Reset :warning:',
+    question: 'Are you sure you want to reset the guild config?\n**EVERYTHING** setup through Zira will be reset.\n\nType `yes` or `no`',
+    yes: 'Guild config has been reset.',
+    no: 'Nothing has been reset.',
+    perm: 'Only the owner of the guild can reset the config.',
+  },
+  list: {
+    title: ' Role List',
+    cont: 'List Continued',
+    null: 'If a message ID is null either reset your config and readd the roles or join the [support server](https://zira.pw/support).',
+    noTitle: 'OwO',
+    noDesc: 'What\'s this?',
+  },
+  remove: {
+    help: ['remove [[message ID](https://support.discordapp.com/hc/en-us/articles/206346498-Where-can-I-find-my-User-Server-Message-ID-)] [@role | role name]** ~~-~~ remove a role that has been setup by Zira\n\nTo get the message ID that a role is on do **', 'list**.'],
+    setup: 'Unable to remove the role as it either was not setup by Zira or is not on the specified message.',
+    removed: ['<@&', '> removed with the emoji '],
+  },
+  edit: {
+    help: 'edit [[message ID](https://support.discordapp.com/hc/en-us/articles/206346498-Where-can-I-find-my-User-Server-Message-ID-)] [new message]** ~~-~~ edit a message sent by Zira',
+    read: ['Unable to get the message in <#', '> because I lack the `Read Messages` or `Read Message History` permission.'],
+    unknownGet: 'Unable to get that message in <#',
+    unknownEdit: 'Unable to edit the message.',
+    no: 'That message can\'t be edited by Zira as it was not sent by Zira.',
+    edited: 'Message has been edited.',
+  },
+};
